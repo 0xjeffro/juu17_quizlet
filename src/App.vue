11ae2eb0aef48b99e613ac0c563e01350706ab1b
Jeffro Tanvik <00002@protonmail.com>
2023-10-05 15:08:15 +0800

    update domain

--- src/App.vue
+++ src/App.vue
@@ -108,7 +108,7 @@ export default {
       let userId = this.$window.Telegram.WebApp["initDataUnsafe"]["user"]["id"]
       let pass = this.calculateScore === 3 ? true : false
       this.axios = axios.create({
-        baseURL: 'https://8725-103-116-72-8.ngrok-free.app',
+        baseURL: 'https://juu7bot.jeffro.io',
         timeout: 5000,
         headers: {'Content-Type': 'application/json'}
       });
